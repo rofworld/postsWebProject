bb8f300253a37ecf6ecdd72c28e9b2c436e9e3c2
Ricardo Belmonte Batlle <Belmonte90@gmail.com>
2018-04-30 21:37:20 +0200

    Version 2

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,5 +1,7 @@
 CREATE SCHEMA postOfToday;
 
+CREATE SCHEMA videoOfToday;
+
 CREATE TABLE postOfToday.posts (
      id MEDIUMINT NOT NULL AUTO_INCREMENT,
      posts VARCHAR(255) UNIQUE NOT NULL,
@@ -20,4 +22,33 @@ CREATE TABLE postOfToday.post_of_the_day(
     post_of_the_day varchar(255) NOT NULL,
     total_rate BIGINT NOT NULL,
     PRIMARY KEY (id)
+);
+
+
+CREATE TABLE videoOfToday.videos (
+     id MEDIUMINT NOT NULL AUTO_INCREMENT,
+     url VARCHAR(255) UNIQUE NOT NULL,
+     path VARCHAR(255) UNIQUE NOT NULL,
+     video_type VARCHAR(100) NOT NULL,
+     total_rate bigint NOT NULL,
+     PRIMARY KEY (id)
+);
+
+CREATE TABLE videoOfToday.best_videos (
+     id MEDIUMINT NOT NULL AUTO_INCREMENT,
+     url VARCHAR(255) UNIQUE NOT NULL,
+     path VARCHAR(255) UNIQUE NOT NULL,
+     video_type VARCHAR(100) NOT NULL,
+     total_rate bigint NOT NULL,
+     PRIMARY KEY (id)
+);
+
+
+CREATE TABLE videoOfToday.video_of_the_day (
+     id MEDIUMINT NOT NULL AUTO_INCREMENT,
+     url VARCHAR(255) UNIQUE NOT NULL,
+     path VARCHAR(255) UNIQUE NOT NULL,
+     video_type VARCHAR(100) NOT NULL,
+     total_rate bigint NOT NULL,
+     PRIMARY KEY (id)
 );
