9a5e5e07f6aa81defee4f0ae460caad4c80643c8
Ricardo Belmonte Batlle <Belmonte90@gmail.com>
2018-09-08 17:23:03 +0200

    Version 2.4

--- postOfToday.php
+++ postOfToday.php
@@ -3,6 +3,8 @@
 
 <head>
 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -11,7 +13,9 @@
 
 
 
-<link rel="stylesheet" href="css/style.css"/>
+<link rel="stylesheet"  media="(min-width: 800px)"  href="css/style.css"/>
+<link rel="stylesheet"  media="(max-width: 800px)"  href="css/m.style.css"/>
+
 <link rel="stylesheet" href="css/rating.css"/>
 
 <title>Post of today ranking</title>
