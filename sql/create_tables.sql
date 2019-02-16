5f1b229e5b88879dbb04ea90baaf5900efccad71
Ricardo Belmonte Batlle <Belmonte90@gmail.com>
2019-02-16 14:53:11 +0100

    Version 3.0 with sql scripts

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,27 +2,7 @@ CREATE SCHEMA postOfToday;
 
 CREATE SCHEMA videoOfToday;
 
-CREATE TABLE postOfToday.posts (
-     id MEDIUMINT NOT NULL AUTO_INCREMENT,
-     posts VARCHAR(255) UNIQUE NOT NULL,
-     total_rate bigint NOT NULL,
-     PRIMARY KEY (id)
-);
 
-CREATE TABLE postOfToday.best_posts (
-     id MEDIUMINT NOT NULL AUTO_INCREMENT,
-     posts VARCHAR(255) UNIQUE NOT NULL,
-     total_rate bigint NOT NULL,
-     PRIMARY KEY (id)
-);
-
-
-CREATE TABLE postOfToday.post_of_the_day(
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-    post_of_the_day varchar(255) NOT NULL,
-    total_rate BIGINT NOT NULL,
-    PRIMARY KEY (id)
-);
 
 
 CREATE TABLE videoOfToday.videos (
@@ -31,6 +11,7 @@ CREATE TABLE videoOfToday.videos (
      path VARCHAR(255) UNIQUE NOT NULL,
      video_type VARCHAR(100) NOT NULL,
      total_rate bigint NOT NULL,
+     uploadDate datetime,
      PRIMARY KEY (id)
 );
 
@@ -40,15 +21,7 @@ CREATE TABLE videoOfToday.best_videos (
      path VARCHAR(255) UNIQUE NOT NULL,
      video_type VARCHAR(100) NOT NULL,
      total_rate bigint NOT NULL,
+     uploadDate datetime,
      PRIMARY KEY (id)
 );
 
-
-CREATE TABLE videoOfToday.video_of_the_day (
-     id MEDIUMINT NOT NULL AUTO_INCREMENT,
-     url VARCHAR(255) UNIQUE NOT NULL,
-     path VARCHAR(255) UNIQUE NOT NULL,
-     video_type VARCHAR(100) NOT NULL,
-     total_rate bigint NOT NULL,
-     PRIMARY KEY (id)
-);
